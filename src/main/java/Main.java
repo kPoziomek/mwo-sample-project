81576ffe106abadf67c34940c7dd84eb1d6d2be9
Soborak <maksymilian.soborak01@gmail.com>
2025-05-10 14:50:24 +0200

    #8 połączenie klasy Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
       System.out.println("Hello World!");
-
+        TestApp testApp = new TestApp();
+        testApp.getWorld();
     }
 
 
